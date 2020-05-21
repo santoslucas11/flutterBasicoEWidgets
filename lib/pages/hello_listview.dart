fee9b0a930d492e55de069115102fabcf6ff24e7
suporte <suporte@mbp-de-suporte.home>
2020-05-21 13:31:40 -0300

    primeiro commit curso de flutter

---
+++ lib/pages/hello_listview.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Dog {
+  String nome;
+  String foto;
+
+  Dog(this.nome, this.foto);
+}
+
+class HelloListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List View"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    List<Dog> dogs = [
+      Dog("Jacku Russel", "assets/images/dog1.png"),
+      Dog("Labrador", "assets/images/dog2.png"),
+      Dog("Pug", "assets/images/dog3.png"),
+      Dog("Rottweiler", "assets/images/dog4.png"),
+      Dog("Pastor", "assets/images/dog5.png"),
+    ];
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: dogs.length,
+      itemBuilder: (context, index) {
+        Dog dog = dogs[index];
+
+        return Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            _img(dog.foto),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.all(4),
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(16),
+                  ),
+                child: Text(
+                  dog.nome,
+                  style: TextStyle(
+                    fontSize: 26, 
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _img(String img) {
+    return Image.asset(
+      img,
+      fit: BoxFit.cover,
+    );
+  }
+}
