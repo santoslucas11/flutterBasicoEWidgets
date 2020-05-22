e17de5cd858f733d1a0e372927f503296c4aa7f2
Lucas Rocha <santoslucas.dev@gmail.com>
2020-05-22 11:13:06 -0300

    drawer adjustments

--- lib/drawer_list.dart
+++ lib/drawer_list.dart
@@ -3,47 +3,49 @@ import 'package:flutter/material.dart';
 class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer( 
-      child: ListView(
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text("User Name"),
-            accountEmail: Text("mail@provider.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/dog4.png"),
+    return SafeArea(
+          child: Drawer( 
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("User Name"),
+              accountEmail: Text("mail@provider.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/dog4.png"),
+              ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.star),
-            title: Text("Favoritos"),
-            subtitle: Text("Mais informações..."),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: (){
-              print("Favoritos");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text("Ajuda"),
-            subtitle: Text("Mais informações..."),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: (){
-              print("Ajuda");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text("Log Out"),
-            subtitle: Text("Mais informações..."),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: (){
-              print("Log Out");
-              Navigator.pop(context);
-            },
-          ),
-        ],),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text("Favoritos"),
+              subtitle: Text("Mais informações..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){
+                print("Favoritos");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text("Ajuda"),
+              subtitle: Text("Mais informações..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){
+                print("Ajuda");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Log Out"),
+              subtitle: Text("Mais informações..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){
+                print("Log Out");
+                Navigator.pop(context);
+              },
+            ),
+          ],),
+      ),
     );
   }
 }
